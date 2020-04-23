10748e85ce754504abc81f792c6ce6c8e2b324da
Vadla Viswanath <viswanathvadla94@gmail.com>
2020-04-23 16:27:20 +0530

    no update

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+import { SubjectComponent } from '../components/subject/subject.component';
 
 
 export const approutes:Routes=[
+       {path:"subject",component:SubjectComponent},
       {path:"ip",loadChildren:'../in-patient/in-patient.module#InPatientModule'},
       {path:"op",loadChildren:'../out-patient/out-patient.module#OutPatientModule'}
   ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,17 @@ import { Ang62Component } from './ang62/ang62.component';
 import { DependentSelectComponent } from './dependent-select/dependent-select.component';
 import { CountryService } from './country.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { SubjectComponent } from './components/subject/subject.component';
+import { Subject2Component } from './components/subject2/subject2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Ang6CompComponent,
     Ang62Component,
-    DependentSelectComponent
+    DependentSelectComponent,
+    SubjectComponent,
+    Subject2Component
   ],
   imports: [
     BrowserModule,
